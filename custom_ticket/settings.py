843726041d73e972c6940dfa73080346b0367943
Cmoyano <cristianmoyano.mza@gmail.com>
2018-03-20 15:21:48 -0300

    additional files and changes for Heroku

--- custom_ticket/settings.py
+++ custom_ticket/settings.py
@@ -121,8 +121,7 @@ DATABASES = {
     }
 }
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
@@ -161,3 +160,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
