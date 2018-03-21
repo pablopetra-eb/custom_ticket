9d69989d8fb4d204b0c4be52258ed4a9e3703304
Cmoyano <cristianmoyano.mza@gmail.com>
2018-03-21 15:31:17 -0300

    add skeleton of models

--- events/models.py
+++ events/models.py
@@ -3,4 +3,34 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+# docs model django 1.11
+# https://docs.djangoproject.com/en/1.11/topics/db/models/
+
+
+class UserModel(models.Model):
+    # By default, Django gives each model the following field:
+    # auto-incrementing primary key.
+    # id = models.AutoField(primary_key=True)
+    field_name = models.CharField(max_length=30)
+
+
+class EventModel(models.Model):
+    # Many-to-one relationships
+    user = models.ForeignKey(UserModel, on_delete=models.CASCADE)
+    pass
+
+
+class TicketTypeModel(models.Model):
+    pass
+
+
+class CustomizationModel(models.Model):
+    pass
+
+
+class TicketTemplateModel(models.Model):
+    pass
+
+
+class EmailConfirmationModel(models.Model):
+    pass
